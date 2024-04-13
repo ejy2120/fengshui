f7ea4f4ee1ebca7d6a4bcefad83cb2b1f2931ade
Ellie Yan <ejy2120@barnard.edu>
2024-04-13 12:58:40 -0400

    Created Flask routes for Concept subpages (concept1.1.html, concept1.2.html); imported images and media layout for concept.1.1 and 1.2)

--- server.py
+++ server.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
+concept_mapping = {
+    '1-1': 'concept1.1.html',
+    '1-2': 'concept1.2.html'
+}
+
 # Load JSON data
-with open('static/concepts.json') as f:
+with open('static/json/concept-about.json') as f:
     concepts_data = json.load(f)
 
 @app.route('/home')
@@ -14,8 +19,9 @@ def home():
 def learn():
     return render_template('learn.html')
 
+# Route for "Concept about" pages
 @app.route('/concept<int:concept_id>')
-def concept(concept_id):
+def concept_about(concept_id):
     concept = None
     for c in concepts_data:
         if c["id"] == concept_id:
@@ -23,9 +29,15 @@ def concept(concept_id):
             break
 
     if concept:
-        return render_template(f'concepts/concept{concept_id}.html', concept=concept)
+        return render_template(f'concepts-about/concept{concept_id}.html', concept=concept)
     else:
         return "Concept not found", 404
+
+# Route for each concepts' example pages 
+# e.g. http://127.0.0.1:5000/concept1-1 takes you to /templates/concept1/concept1.1.html
+@app.route('/concept<int:concept_id>-<int:example_page_id>')
+def concept_example(concept_id, example_page_id):
+    return render_template(f'concept{concept_id}/concept{concept_id}.{example_page_id}.html')
     
 @app.route('/quiz')
 def quiz():
