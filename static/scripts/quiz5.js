c6cec7f3f6f61e1a7affe59d8f86c70e95842e8c
michaelagary <michaelanicole03@gmail.com>
2024-04-17 12:15:21 -0400

    quiz 5 done (button format issues)

--- static/scripts/quiz5.js
+++ static/scripts/quiz5.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-    $('#select-option1').click(function() {
-        $('#option1').css('border', '2px solid red');
-        $('#feedback-message').html("Try again! This option is not aligned with <span class='red-text'><b>our principles</b></span>.")
+    $('#select-clay').click(function() {
+        $('#clay-vase').css('border', '2px solid green');
+        $('#feedback-message').html("Correct! These vases are made of <span class='green-text'><b>clay</b></span>, a <span class='green-text'><b>natural material</b></span> which is connected to the <span class='green-text'><b>earth element</b></span> and brings a feeling of <span class='green-text'><b>groundedness</b></span>.")
+                              .css('background-color', '#DDEEEA')
+                              .show();
+        $('#try-again-button').hide();
+        $('#next-question-button').show();
+    });
+    $('#select-brass').click(function() {
+        $('#brass-vase').css('border', '2px solid red');
+        $('#feedback-message').html("Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability.")
                               .css('background-color', '#F7DAD2')
                               .show();
         $('#try-again-button').show();
         $('#next-question-button').hide();
     });
-
-    $('#select-option2').click(function() {
-        $('#option2').css('border', '2px solid green');
-        $('#feedback-message').html("Correct! This option aligns perfectly with <span class='green-text'><b>our goals</b></span>.")
-                              .css('background-color', '#DDEEEA')
+    $('#select-steel').click(function() {
+        $('#steel-vase').css('border', '2px solid red');
+        $('#feedback-message').html("Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability.")
+                              .css('background-color', '#F7DAD2')
                               .show();
-        $('#try-again-button').hide();
-        $('#next-question-button').show();
+        $('#try-again-button').show();
+        $('#next-question-button').hide();
     });
 });
