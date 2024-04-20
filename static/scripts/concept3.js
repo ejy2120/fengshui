906a23c1f42fb07591d0a70203269b3fd11cd85d
Salia Nahshal <snn2126@barnard.edu>
2024-04-20 10:59:37 -0400

    completed symmetry & chi concepts

--- static/scripts/concept3.js
+++ static/scripts/concept3.js
@@ -1,74 +1,78 @@
 $(document).ready(function () {
-  // Define the correct and wrong positions
-  var correctPosition = $(".chi-op2");
-  var wrongPosition = $(".wrong-position");
+  let originalText = $(".text-bubble3").text();
+  let originalBackgroundColor = $(".text-bubble3").css("background-color");
 
-  // Store the original text and background color
-  var originalText = $(".text-bubble3").text();
-  var originalBackgroundColor = $(".text-bubble3").css("background-color");
-  var originalMarginTop = $(".text-bubble3").css("margin-top");
+  let bedDraggable = true;
 
-  // Make the mirror image draggable
-  $("#mirror").draggable({
+  // Make the bed image draggable
+  $(".draggable").draggable({
     revert: "invalid",
     cursor: "move",
-    containment: "#chi-room",
+    containment: "document",
     zIndex: 1000,
   });
 
-  // Define droppable areas
-  correctPosition.droppable({
+  // Make the correct and incorrect divs droppable
+  $(".correct-div, .incorrect-div").droppable({
+    accept: ".draggable", // Only accept draggable items
     drop: function (event, ui) {
-      $(this).addClass("correct-drop");
-      $("#next-concept-button").show();
-      $("#try-again-button").hide();
-      $(".text-bubble3").html(
-        "Great! The mirror will <span class='green-text'> amplify the natural light and views</span> that the window offers, boosting positive chi."
-      );
-      $(".text-bubble3").css({
-        "background-color": "#DDEEEA",
-        "margin-top": "-20%",
-      });
-      $(".green-text").css({
-        color: "#19676B",
-        "font-weight": "bold",
-      });
+      var droppable = $(this);
+      console.log("Dropped on: ", droppable.attr("class"));
+
+      if (droppable.hasClass("correct-div")) {
+        $("#next-concept-button").show();
+
+        $("#try-again-button").hide();
+
+        // If dropped into the correct div, show a good message
+        $(".text-bubble3").html(
+          "Great! The mirror will <span class='green-text'>amplify the natural light and views</span> that the window offers, boosting positive chi."
+        );
+
+        $(".text-bubble3").css("background-color", "#DDEEEA");
+
+        $(".green-text").css({ color: "#19676B", "font-weight": "bold" });
+
+        $(
+          "img[src='{{ url_for('static', filename='images/com-selfcheck.png') }}']"
+        ).attr(
+          "src",
+          "{{ url_for('static', filename='images/com-selfcheck-right.png') }}"
+        );
+      } else if (droppable.hasClass("incorrect-div")) {
+        $("#next-concept-button").hide();
+
+        $("#try-again-button").show();
+
+        // If dropped into the incorrect div, show a bad message
+        $(".text-bubble3").html(
+          "Even though the mirror somewhat reflects beautiful art, the mirror  <span class='red-text'>reflects positive chi out</span> of the room."
+        );
+
+        $(".text-bubble3").css("background-color", "#F7DAD2");
+
+        $(".red-text").css({ color: "#E06666", "font-weight": "bold" });
+
+        bedDraggable = false; // Disable draggable behavior so user cannot drag bed to correct location right after
+        $(".draggable").draggable("disable"); // Disable draggable behavior
+      }
     },
   });
 
-  wrongPosition.droppable({
-    drop: function (event, ui) {
-      $(this).addClass("incorrect-drop");
-      $("#try-again-button").show();
-      $("#next-concept-button").hide();
-      $(".text-bubble3").html(
-        "Though the mirror reflects beautiful art it <span class='red-text'>emits positive chi</span> from the room."
-      );
-      $(".text-bubble3").css({
-        "background-color": "#F7DAD2",
-        "margin-top": "-20%",
-      });
-      $(".red-text").css({
-        color: "#E06666",
-        "font-weight": "bold",
-      });
-    },
-  });
-
-  // Reset button functionality
   $("#try-again-button").click(function () {
-    $(".droppable").removeClass("correct-drop incorrect-drop");
-    $("#mirror").css({ top: 0, left: 0 });
-    $(this).hide();
+    $(".draggable").css({ top: 0, left: 0 });
+    $("#try-again-button").hide();
+    $("#next-concept-button").hide();
+    $(".text-bubble3").text("");
 
-    // Restore the original text, color, background color, and margin top
     $(".text-bubble3").text(originalText).css({
       "background-color": originalBackgroundColor,
-      "margin-top": originalMarginTop,
     });
+
+    bedDraggable = true; // Enable draggable behavior
+    $(".draggable").draggable("enable"); // Enable draggable behavior
   });
 
-  // Next concept button functionality
   $("#next-concept-button").click(function () {
     window.location.href = "/concept4";
   });
