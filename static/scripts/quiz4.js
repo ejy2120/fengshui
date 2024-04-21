fa9db86fae6b93102805e7c4e04fc14f070f014f
Salia Nahshal <snn2126@barnard.edu>
2024-04-20 22:10:19 -0400

    updated javascript for quiz 4 and 5

--- static/scripts/quiz4.js
+++ static/scripts/quiz4.js
@@ -1,43 +1,69 @@
-$(document).ready(function() {
-    var questionId = "quiz4"; // Unique identifier for this quiz question
+$(document).ready(function () {
+  var questionId = "quiz4"; // Unique identifier for this quiz question
 
-    $('#select-polyester').click(function() {
-        markAttempted(questionId); // Mark this question as attempted
-        $('#polyester-rug').css('border', '3px solid #e06666ff');
-        $('#polyester-rug').css('border-radius', '20px');
-        $('#feedback-message').html("Choose a rug made of <span class='red-text'><b>natural material</b></span> to align with the earth element.")
-                              .css('background-color', '#F7DAD2')
-                              .css('width', '100%')
-                              .css('margin-left', '30px')
-                              .show();
-        $('#try-again-button').show();
-        $('#next-question-button').hide();
-    });
+  $("#select-polyester").click(function () {
+    markAttempted(questionId); // Mark this question as attempted
+    $("#polyester-rug").css("border", "3px solid #e06666ff");
+    $("#polyester-rug").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Choose a rug made of <span class='red-text'><b>natural material</b></span> to align with the earth element."
+      )
+      .css("background-color", "#F7DAD2")
+      .css("width", "100%")
+      .css("margin-left", "30px")
+      .show();
+    $("#try-again-button").show();
+    $("#next-question-button").hide();
+    // Disable other select options
+    $("#select-cotton, #select-fiber").prop("disabled", true);
+  });
 
-    $('#select-cotton').click(function() {
-        markAttempted(questionId); // Mark this question as attempted
-        $('#cotton-rug').css('border', '3px solid #e06666ff');
-        $('#cotton-rug').css('border-radius', '20px');
-        $('#feedback-message').html("Choose a rug with a <span class='red-text'><b>balanced/soft shape</b></span> to align with the earth element.")
-                              .css('background-color', '#F7DAD2')
-                              .css('width', '100%')
-                              .css('margin-left', '30px')
-                              .show();
-        $('#try-again-button').show();
-        $('#next-question-button').hide();
-    });
+  $("#select-cotton").click(function () {
+    markAttempted(questionId); // Mark this question as attempted
+    $("#cotton-rug").css("border", "3px solid #e06666ff");
+    $("#cotton-rug").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Choose a rug with a <span class='red-text'><b>balanced/soft shape</b></span> to align with the earth element."
+      )
+      .css("background-color", "#F7DAD2")
+      .css("width", "100%")
+      .css("margin-left", "30px")
+      .show();
+    $("#try-again-button").show();
+    $("#next-question-button").hide();
+    // Disable other select options
+    $("#select-polyester, #select-fiber").prop("disabled", true);
+  });
 
-    $('#select-fiber').click(function() {
-        correctAnswer(questionId); // Increase the score
-        
-        $('#fiber-rug').css('border', '3px solid #83a29d');
-        $('#fiber-rug').css('border-radius', '20px');
-        $('#feedback-message').html("Correct! This rug is made of <span class='green-text'><b>natural material</b></span> and has a <span class='green-text'><b>balanced shape</b></span>, which promotes the earth element’s <span class='green-text'><b>stability</b></span>.")
-                              .css('background-color', '#DDEEEA')
-                              .css('width', '100%')
-                              .css('margin-left', '30px')
-                              .show();
-        $('#try-again-button').hide();
-        $('#next-question-button').show();
-    });
+  $("#select-fiber").click(function () {
+    if (!isAttempted(questionId)) {
+      // Check if it's the first correct attempt
+      correctAnswer(questionId); // Increment the score
+    }
+    $("#fiber-rug").css("border", "3px solid #83a29d");
+    $("#fiber-rug").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Correct! This rug is made of <span class='green-text'><b>natural material</b></span> and has a <span class='green-text'><b>balanced shape</b></span>, which promotes the earth element’s <span class='green-text'><b>stability</b></span>."
+      )
+      .css("background-color", "#DDEEEA")
+      .css("width", "100%")
+      .css("margin-left", "30px")
+      .show();
+    $("#try-again-button").hide();
+    $("#next-question-button").show();
+    // Disable other select options
+    $("#select-polyester, #select-cotton").prop("disabled", true);
+  });
+
+  $("#try-again-button").click(function () {
+    // Reset styles and enable all select options
+    $(".rug-select").css("border", "none");
+    $(".rug-select").prop("disabled", false);
+    $("#feedback-message").hide();
+    $("#try-again-button").hide();
+    $("#next-question-button").hide();
+  });
 });
--- static/scripts/quiz5.js
+++ static/scripts/quiz5.js
@@ -1,35 +1,55 @@
-$(document).ready(function() {
-    var questionId = "quiz5"; // Unique identifier for this quiz question
+$(document).ready(function () {
+  var questionId = "quiz5"; // Unique identifier for this quiz question
 
-    $('#select-clay').click(function() {
-        correctAnswer(questionId); // Increase the score
-        
-        $('#clay-vase').css('border', '3px solid #83a29d');
-        $('#clay-vase').css('border-radius', '20px');
-        $('#feedback-message').html("Correct! These vases are made of <span class='green-text'><b>clay</b></span>, a <span class='green-text'><b>natural material</b></span> which is connected to the <span class='green-text'><b>earth element</b></span> and brings a feeling of <span class='green-text'><b>groundedness</b></span>.")
-                              .css('background-color', '#DDEEEA')
-                              .show();
-        $('#try-again-button').hide();
-        $('#next-question-button').show();
-    });
-    $('#select-brass').click(function() {
-        markAttempted(questionId); // Mark this question as attempted
-        $('#brass-vase').css('border', '3px solid #e06666ff');
-        $('#brass-vase').css('border-radius', '20px');
-        $('#feedback-message').html("Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability.")
-                              .css('background-color', '#F7DAD2')
-                              .show();
-        $('#try-again-button').show();
-        $('#next-question-button').hide();
-    });
-    $('#select-steel').click(function() {
-        markAttempted(questionId); // Mark this question as attempted
-        $('#steel-vase').css('border', '3px solid #e06666ff');
-        $('#steel-vase').css('border-radius', '20px');
-        $('#feedback-message').html("Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability.")
-                              .css('background-color', '#F7DAD2')
-                              .show();
-        $('#try-again-button').show();
-        $('#next-question-button').hide();
-    });
+  $("#select-clay").click(function () {
+    if (!isAttempted(questionId)) {
+      // Check if it's the first correct attempt
+      correctAnswer(questionId); // Increment the score
+    }
+    // Disable other options
+    $("#select-brass, #select-steel").prop("disabled", true);
+
+    $("#clay-vase").css("border", "3px solid #83a29d");
+    $("#clay-vase").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Correct! These vases are made of <span class='green-text'><b>clay</b></span>, a <span class='green-text'><b>natural material</b></span> which is connected to the <span class='green-text'><b>earth element</b></span> and brings a feeling of <span class='green-text'><b>groundedness</b></span>."
+      )
+      .css("background-color", "#DDEEEA")
+      .show();
+    $("#try-again-button").hide();
+    $("#next-question-button").show();
+  });
+  $("#select-brass").click(function () {
+    markAttempted(questionId); // Mark this question as attempted
+    // Disable other options
+    $("#select-clay, #select-steel").prop("disabled", true);
+
+    $("#brass-vase").css("border", "3px solid #e06666ff");
+    $("#brass-vase").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability."
+      )
+      .css("background-color", "#F7DAD2")
+      .show();
+    $("#try-again-button").show();
+    $("#next-question-button").hide();
+  });
+  $("#select-steel").click(function () {
+    markAttempted(questionId); // Mark this question as attempted
+    // Disable other options
+    $("#select-clay, #select-brass").prop("disabled", true);
+
+    $("#steel-vase").css("border", "3px solid #e06666ff");
+    $("#steel-vase").css("border-radius", "20px");
+    $("#feedback-message")
+      .html(
+        "Try again! We don’t want items made of <span class='red-text'><b>metal</b></span> – that <span class='red-text'><b>clashes</b></span> with earth element and feeling of stability."
+      )
+      .css("background-color", "#F7DAD2")
+      .show();
+    $("#try-again-button").show();
+    $("#next-question-button").hide();
+  });
 });
